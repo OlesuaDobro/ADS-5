81c9e059b03fb8e410f0fa2109f473d65e53b2f6
Dobro <76173176+OlesuaDobro@users.noreply.github.com>
2024-04-04 18:59:46 +0300

    Update alg.cpp
    sorry

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,69 +2,11 @@
 #include <string>
 #include <map>
 #include "tstack.h"
+// преобразование выражение в постфиксную форму
 std::string infx2pstfx(std::string inf) {
-  std::map<char, int> priority;
-  priority['+'] = 1;
-  priority['-'] = 1;
-  priority['*'] = 2;
-  priority['/'] = 2;
-  priority['^'] = 3;
-  std::string postExp = "";
-  TStack<char> opStack;
-  for (char c : inf) {
-    if (isalnum(c)) {
-      postExp += c;
-    } else if (c == '(') {
-      opStack.push(c);
-    }
-    else if (c == ')') {
-      while (!opStack.isEmpty() && opStack.top() != '(') {
-        postExp += opStack.top();
-        opStack.pop();
-      }
-      opStack.pop();
-    } else {
-      while (!opStack.isEmpty() && opStack.top() != '(' &&
-          priority[opStack.top()] >= priority[c]) {
-        postExp += opStack.top();
-        opStack.pop();
-      }
-      opStack.push(c);
-    }
-  }
-  while (!opStack.isEmpty()) {
-    postExp += opStack.top();
-    opStack.pop();
-  }
-  return postExp;
+
 }
+// вычисление выражения, записанного в постфиксной форме
 int eval(std::string post) {
-  TStack<int> numStack;
-  for (char c : post) {
-    if (isdigit(c)) {
-      numStack.push(c - '0');
-    } else {
-      int num2 = numStack.top();
-      numStack.pop();
-      int num1 = numStack.top();
-      numStack.pop();
-      switch (c) {
-      case '+':
-        numStack.push(num1 + num2);
-        break;
-      case '-':
-        numStack.push(num1 - num2);
-        break;
-      case '*':
-        numStack.push(num1 * num2);
-        break;
-      case '/':
-        numStack.push(num1 / num2);
-        break;
-      case '^':
-        numStack.push(pow(num1, num2));
-        break;
-      }
-    }
-  }
-  return numStack.top();
+
+}
