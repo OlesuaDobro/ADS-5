27fd93285bf2f323e444aee2a8fbcbea25762212
Dobro <76173176+OlesuaDobro@users.noreply.github.com>
2024-04-04 18:01:07 +0300

    1 tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -4,7 +4,22 @@
 
 template<typename T, int size>
 class TStack {
-  // добавьте код стека
+  private:
+Tdata[size]:
+publick:
+TStack():top(0){}
+void push(count T&item) {
+  if (top <= size-1)
+    data[top++]=item;
+  else resize(2*size);
+  throw std:string("Full");
+  Tpop(){
+    if(top>0)
+      return data[--top];
+    else
+      throw
+        } std::string("Empty");
 };
-
+}
+}
 #endif  // INCLUDE_TSTACK_H_
