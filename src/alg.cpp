5c866667717b240c005d450e105d0c985c0149b9
Dobro <76173176+OlesuaDobro@users.noreply.github.com>
2024-04-05 21:10:55 +0300

    25Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,9 +4,9 @@
 #include "tstack.h"
 
 int getPriority(char op) {
-    if(op == '+' || op == '-') {
+    if (op == '+' || op == '-') {
         return 1;
-    } else if(op == '*' || op == '/') {
+    } else if (op == '*' || op == '/') {
         return 2;
     } else {
         return 0;
@@ -15,40 +15,40 @@ int getPriority(char op) {
 std::string infx2pstfx(std::string inf) {
     std::string postfix;
     std::Tstack<char> operatorStack;
-    for(int i = 0; i < inf.length(); i++) {
+    for (int i = 0; i < inf.length(); i++) {
         char c = inf[i];
-        if(isdigit(c)) {
+        if (isdigit(c)) {
             postfix += c;
-        } else if(c == '+' || c == '-' || c == '*' || c == '/') {
-            while(!operatorStack.empty() && 
-                  getPriority(operatorStack.top()) >= getPriority(c)) {
+        } else if (c == '+' || c == '-' || c == '*' || c == '/') {
+            while (!operatorStack.empty() && 
+                  getPriority (operatorStack.top()) >= getPriority(c)) {
                 postfix += operatorStack.top();
                 operatorStack.pop();
             }
             operatorStack.push(c);
-        } else if(c == '(') {
+        } else if (c == '(') {
             operatorStack.push(c);
-        } else if(c == ')') {
-            while(!operatorStack.empty() && operatorStack.top() != '(') {
+        } else if (c == ')') {
+            while (!operatorStack.empty() && operatorStack.top() != '(') {
                 postfix += operatorStack.top();
                 operatorStack.pop();
             }
-            if(!operatorStack.empty() && operatorStack.top() == '(') {
+            if (!operatorStack.empty() && operatorStack.top() == '(') {
                 operatorStack.pop();
             }
         }
     }
-    while(!operatorStack.empty()) {
+    while (!operatorStack.empty()) {
         postfix += operatorStack.top();
         operatorStack.pop();
     }
     return postfix;
 }
-int eval(std::string post) {
+int eval (std::string post) {
     std::Tstack<int> operandStack;
-    for(int i = 0; i < post.length(); i++) {
+    for (int i = 0; i < post.length(); i++) {
         char c = post[i];
-        if(isdigit(c)) {
+        if (isdigit(c)) {
             operandStack.push(c - '0');
         } else if(c == '+' || c == '-' || c == '*' || c == '/') {
             int operand2 = operandStack.top();
@@ -56,7 +56,7 @@ int eval(std::string post) {
             int operand1 = operandStack.top();
             operandStack.pop();
             int result;
-            switch(c) {
+            switch (c) {
                 case '+':
                     result = operand1 + operand2;
                     break;
