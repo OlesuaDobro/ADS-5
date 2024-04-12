1472dfedb136542d8daa715fd8c77593918e285f
Dobro <76173176+OlesuaDobro@users.noreply.github.com>
2024-04-12 18:42:06 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -75,11 +75,18 @@ if (!stack1.isEmpty()) {
         postfix += ' ';
     }
 }
-int eval(std::string post) {
+    std::string postfix2;
+    for (int i = 0; i < (postfix.length() - 1); i++) {
+        postfix2 += postfix[i];
+    }
+    return postfix2;
+      }
+int eval(std::string pref) {
     TStack<int, 100> stack2;
     for (int i = 0; i < pref.length(); i++) {
         if ((pref[i] >= '0') && (pref[i] <= '9')) {
             stack2.push(pref[i] - '0');
+          } else if (pref[i] != ' ' || (pref[i] < '0') && (pref[i] > '9')) {
             int operand2 = stack2.top();
             int operand1 = stack2.top();
             switch (pref(i)) {
