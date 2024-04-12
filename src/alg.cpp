0a842d9c7e69d587f65cf98350198c43b2392d9a
Dobro <76173176+OlesuaDobro@users.noreply.github.com>
2024-04-12 19:33:52 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -92,7 +92,7 @@ int eval(std::string pref) {
     for (int i = 0; i < pref.length(); i++) {
         if ((pref[i] >= '0') && (pref[i] <= '9')) {
             stack2.push(pref[i] - '0');
-          } else if (pref[i] != ' ' || (pref[i] < '0') && (pref[i] > '9')) {
+        } else if (pref[i] != ' ' || (pref[i] < '0') && (pref[i] > '9')) {
             int operand2 = stack2.pop();
             int operand1 = stack2.pop();
             switch (pref(i)) {
